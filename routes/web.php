1f1b28ce691087b9c9f9c2d0429cc3832f4a3ea7
Chamikara Bandara <113469394+chamikarab@users.noreply.github.com>
2024-09-21 23:56:50 +0530

    Revert "add: Project page"

--- routes/web.php
+++ routes/web.php
@@ -2,27 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-
-
 Route::get('/', function () {
-    return view('home');
-})->name('home');
-
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
-
-Route::get('/projects', function () {
-    return view('projects');
-})->name('projects');
-
-Route::get('/contact', function () {
-    return view('contact');
-})->name('contact');
-
-Route::post('/contact/submit', [ContactController::class, 'submit'])->name('contact.submit');
-
+    return view('welcome');
+});
