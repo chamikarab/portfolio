55759dd181779bc4255875ea261fec93cd361c39
chamikarab <chamikara38@gmail.com>
2025-12-24 16:27:32 +0530

    Implement custom maintenance mode with middleware for admin access control

--- public/index.php
+++ public/index.php
@@ -1,13 +1,11 @@
 <?php
 
+use Illuminate\Http\Request;
+
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
-// Allow admin routes to bypass maintenance mode so admins can always access login and dashboard
-$requestUri = parse_url($_SERVER['REQUEST_URI'] ?? '/', PHP_URL_PATH);
-$isAdminRoute = strpos($requestUri, '/admin') === 0 || $requestUri === '/login';
-
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php') && !$isAdminRoute) {
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
