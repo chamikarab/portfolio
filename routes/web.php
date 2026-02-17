d35b6c3f96a74810182e7a855b6fe8405e9a2915
chamikarab <chamikara38@gmail.com>
2026-02-17 11:15:56 +0530

    refactor: simplify XML generation in sitemap route

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\TestimonialController;
 
 // Sitemap
 Route::get('/sitemap.xml', function () {
+
     $baseUrl = rtrim(config('app.url'), '/');
     $now = now()->format('Y-m-d');
 
@@ -19,25 +20,23 @@ Route::get('/sitemap.xml', function () {
         ['loc' => $baseUrl . '/contact', 'priority' => '0.8'],
     ];
 
-    $xml = '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
-    $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n";
+    $xml = '<?xml version="1.0" encoding="UTF-8"?>';
+    $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 
     foreach ($urls as $entry) {
-        $xml .= '  <url>' . "\n";
-        $xml .= '    <loc>' . e($entry['loc']) . '</loc>' . "\n";
-        $xml .= '    <lastmod>' . $now . '</lastmod>' . "\n";
-        $xml .= '    <changefreq>monthly</changefreq>' . "\n";
-        $xml .= '    <priority>' . $entry['priority'] . '</priority>' . "\n";
-        $xml .= '  </url>' . "\n";
+        $xml .= '<url>';
+        $xml .= '<loc>' . $entry['loc'] . '</loc>';
+        $xml .= '<lastmod>' . $now . '</lastmod>';
+        $xml .= '<changefreq>monthly</changefreq>';
+        $xml .= '<priority>' . $entry['priority'] . '</priority>';
+        $xml .= '</url>';
     }
 
     $xml .= '</urlset>';
 
-    return response($xml, 200, [
-        'Content-Type' => 'application/xml',
-        'Cache-Control' => 'public, max-age=3600',
-    ]);
-})->name('sitemap');
+    return response($xml, 200)
+        ->header('Content-Type', 'application/xml');
+});
 
 // Maintenance page route
 Route::get('/maintenance', function () {
